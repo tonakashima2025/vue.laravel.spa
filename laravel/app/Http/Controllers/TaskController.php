ae7bf1d576f48f1e2a4c5057942e4c0c2d2509de
tonakashima2025 <tonakashima2025@gmail.com>
2021-07-24 22:23:45 +0900

    initial commit

---
+++ laravel/app/Http/Controllers/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index() {
+
+        return Task::all();
+    
+    }
+
+    public function store(Request $request) {
+
+        return Task::create($request->all());
+    
+    }
+
+    public function show(Task $task) {
+
+        return $task;
+    
+    }
+
+    public function update(Request $request, Task $task) {
+
+        $task->update($request->all());
+
+        return $task;
+    
+    }
+
+    public function destroy(Task $task) {
+
+        $task->delete();
+
+        return $task;
+    
+    }
+}
---
+++ laravel/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::get('/tasks/{task}', 'TaskController@show');
+Route::put('/tasks/{task}', 'TaskController@update');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
